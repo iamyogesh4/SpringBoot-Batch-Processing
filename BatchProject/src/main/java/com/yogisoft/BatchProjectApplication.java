e2fc96589905ab4d7ee391dd610dd222a1030494
yogesh <yogeshgawali141@gmail.com>
2024-02-25 15:19:37 +0530

    First Version Change Commit

---
+++ BatchProject/src/main/java/com/yogisoft/BatchProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yogisoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchProjectApplication.class, args);
+	}
+
+}

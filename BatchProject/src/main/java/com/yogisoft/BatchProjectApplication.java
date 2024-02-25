6922691e049d77de38d7f0c78ebff28fb3e495e8
yogesh <yogeshgawali141@gmail.com>
2024-02-25 15:48:40 +0530

    Hi Sachin Commit

--- BatchProject/src/main/java/com/yogisoft/BatchProjectApplication.java
+++ BatchProject/src/main/java/com/yogisoft/BatchProjectApplication.java
@@ -9,7 +9,7 @@ public class BatchProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BatchProjectApplication.class, args);
 		
-		System.out.println("Hi ");
+		System.out.println("Hi Sachin ");
 	}
 
 }
